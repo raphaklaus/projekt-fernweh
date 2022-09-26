dbcf05d41d862ef152c7296a01970fbf91473c45
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-09-26 23:55:47 +0200

    Remove unused import

--- src/scripts/helpers/streamer.ts
+++ src/scripts/helpers/streamer.ts
@@ -1,23 +1,21 @@
-import Batch from 'stream-json/utils/Batch';
-import { parser } from 'stream-json/jsonl/Parser'
-import StreamArray from 'stream-json/streamers/StreamArray';
-import { chain } from 'stream-chain';
-import { Sentence } from '../../interfaces/sentences';
-import fs from 'fs'
+import Batch from "stream-json/utils/Batch";
+import { parser } from "stream-json/jsonl/Parser";
+import { chain } from "stream-chain";
+import { Sentence } from "../../interfaces/sentences";
+import fs from "fs";
 
-const batchSize = 500
+const batchSize = 500;
 
-export const stream = (filePath:string, bulkInsert:Function) => {
+export const stream = (filePath: string, bulkInsert: Function) => {
   const pipeline = chain([
     fs.createReadStream(filePath),
     parser(),
-    // StreamArray.withParser(),
-    new Batch({batchSize})
+    new Batch({ batchSize }),
   ]);
 
-  pipeline.on('data', (data:Sentence[]) => {
-    console.log(`Importing ${data.length} entities...`)
+  pipeline.on("data", (data: Sentence[]) => {
+    console.log(`Importing ${data.length} entities...`);
     bulkInsert(data);
   });
-  pipeline.on('end', () => console.log("Import completed"));
-}
+  pipeline.on("end", () => console.log("Import completed"));
+};
