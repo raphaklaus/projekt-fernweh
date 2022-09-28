424f6fad84bc4d4898a2c4286a797589b479da54
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-09-28 11:18:25 +0200

    Add repo functions for REST API

--- src/repo/firestore/config.ts
+++ src/repo/firestore/config.ts
@@ -1,14 +1,14 @@
-import { initializeApp, cert } from "firebase-admin/app"
-import { getFirestore } from "firebase-admin/firestore"
-import fs from 'fs'
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import fs from "fs";
 
 export const getDBInstance = () => {
-  const serviceAccountRaw = fs.readFileSync("firebase-key.json", 'utf8')
-  const serviceAccount = JSON.parse(serviceAccountRaw)
+  const serviceAccountRaw = fs.readFileSync("firebase-key.json", "utf8");
+  const serviceAccount = JSON.parse(serviceAccountRaw);
 
   initializeApp({
-    credential: cert(serviceAccount)
-  })
+    credential: cert(serviceAccount),
+  });
 
-  return getFirestore()
-}
+  return getFirestore();
+};
--- src/repo/firestore/repo.ts
+++ src/repo/firestore/repo.ts
@@ -1,13 +1,28 @@
-import { Sentence } from "../../interfaces/sentences";
 import { SentenceJSONL } from "../../interfaces/sentences_jsonl";
 import { getDBInstance } from "./config";
 import { map } from "../mappers/sentences";
 
 const db = getDBInstance();
 
-export const bulkInsert = async (entities: SentenceJSONL[]) => {
+export const get = async () => {
+  const sentencesRef = db.collection("sentences");
+  const snapshots = await sentencesRef
+    .orderBy("text")
+    .startAt(0)
+    .limit(10)
+    .get();
+
+  return snapshots.docs.map((x) => x.data());
+};
+
+export const bulkInsert = async (
+  entities: {
+    key: number;
+    value: SentenceJSONL;
+  }[]
+) => {
   const batch = db.batch();
-  const mapped_entities = map(entities);
+  const mapped_entities = map(entities.map((x) => x.value));
 
   mapped_entities.map(async (entity) => {
     const ref = db.collection("sentences").doc();
