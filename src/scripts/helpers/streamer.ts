77bb63b4df5db3be4d4b5d199af34361c0d3255b
Raphael D. Pinheiro <raphaklaus@gmail.com>
2022-09-28 11:20:16 +0200

    Improve streaming module

--- src/scripts/helpers/streamer.ts
+++ src/scripts/helpers/streamer.ts
@@ -1,31 +1,43 @@
 import Batch from "stream-json/utils/Batch";
-import { parser } from "stream-json/jsonl/Parser";
+import { parser as parserJSONL } from "stream-json/jsonl/Parser";
 import { chain } from "stream-chain";
-import { Sentence } from "../../interfaces/sentences";
 import fs from "fs";
+import { SentenceJSONL } from "../../interfaces/sentences_jsonl";
+import { IMap } from "./map";
+
+type StreamOpts = {
+  count: (text: string, state: IMap<string, number>) => IMap<string, number>;
+  rank: (
+    countedWords: IMap<string, number>,
+    limit: number
+  ) => IMap<string, number>;
+  print: (rankedWords: IMap<string, number>) => void;
+};
+
+export const stream = (filePath: string, opts: StreamOpts) => {
+  const pipeline = chain([fs.createReadStream(filePath), parserJSONL()]);
+
+  let accumulator = new Map<string, number>() as IMap<string, number>;
+  pipeline.on("data", (data: { key: number; value: SentenceJSONL }) => {
+    console.log(data.key);
+    accumulator = opts.count(data.value.text, accumulator);
+  });
+  pipeline.on("end", () => {
+    const ranked = opts.rank(accumulator, 100);
+    opts.print(ranked);
+  });
+};
 
 const batchSize = 500;
-
-export const stream = (filePath: string, fn: Function) => {
-  const pipeline = chain([fs.createReadStream(filePath), parser()]);
-  let count = 0;
-
-  pipeline.on("data", (data: Sentence) => {
-    data.text.split(" ").reduce(prev, (cur) => {
-      prev;
-    });
-  });
-  pipeline.on("end", () => console.log("Count completed"));
-};
 
 export const streamBatch = (filePath: string, fn: Function) => {
   const pipeline = chain([
     fs.createReadStream(filePath),
-    parser(),
+    parserJSONL(),
     new Batch({ batchSize }),
   ]);
 
-  pipeline.on("data", (data: Sentence[]) => {
+  pipeline.on("data", (data: SentenceJSONL[]) => {
     console.log(`Importing ${data.length} entities...`);
     fn(data);
   });
